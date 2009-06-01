2ee02914f85d6a89fd47b88b2f523626e72b4ef5
Lukasz.Rajchel <none@none>
2009-06-01 12:30:16 +0000

    tests updated
    --HG--
    extra : convert_revision : svn%3A7d215080-934a-0410-a889-27081d6b4ed7/trunk%4082

--- tests/tests.js
+++ tests/tests.js
@@ -6,8 +6,20 @@ $(document).ready(function () {
 	var elem;
 	
 	test("Basic element creation", function () {
-		var elem = $.create('div');
+		elem = $.create('div');
 		ok(elem.get(0).tagName === "DIV", "We expect DIV element to be created");
+		elem = $.create(null);
+		ok(elem === undefined, "We expect malformed element parameter to be ignored (null)");
+		elem = $.create(undefined);
+		ok(elem === undefined, "We expect malformed element parameter to be ignored (undefined)");
+		elem = $.create({one: 'div'});
+		ok(elem === undefined, "We expect malformed element parameter to be ignored (object)");
+		elem = $.create(['div']);
+		ok(elem === undefined, "We expect malformed element parameter to be ignored (array)");
+		elem = $.create(10);
+		ok(elem === undefined, "We expect malformed element parameter to be ignored (number)");
+		elem = $.create(true);
+		ok(elem === undefined, "We expect malformed element parameter to be ignored (boolean)");
 	});
 	
 	test("Attributes", function () {
@@ -16,8 +28,16 @@ $(document).ready(function () {
 		elem = $.create('div', {id: 'myId', name: 'myName'});
 		ok(elem.attr('id') === "myId", "(Multiple attributes) We expect element's identifier of myId");
 		ok(elem.attr('name') === "myName", "(Multiple attributes) We expect element's name of myName");
+		elem = $.create('div', null);
+		ok(elem.attr('id') === "", "We expect null attributes parameter to be ignored");
+		elem = $.create('div', ['id: myId']);
+		ok(elem.attr('id') === "", "We expect malformed attributes parameter to be ignored (array)");
 		elem = $.create('div', 'id: myId');
-		ok(elem.attr('id') === "", "We expect malformed attributes parameter to be ignored");
+		ok(elem.attr('id') === "", "We expect malformed attributes parameter to be ignored (string)");
+		elem = $.create('div', 10);
+		ok(elem.attr('id') === "", "We expect malformed attributes parameter to be ignored (number)");
+		elem = $.create('div', true);
+		ok(elem.attr('id') === "", "We expect malformed attributes parameter to be ignored (boolean)");
 	});
 	
 	test("Children", function () {
@@ -25,13 +45,19 @@ $(document).ready(function () {
 		ok(elem.text() === "myText", "We expect element's text to be set to myText");
 		elem = $.create('div', null, $.create('p'));
 		ok(elem.children().get(0).tagName === "P", "(Single child) We expect element to have P child element");
+		elem = $.create('div', null, [$.create('p')]);
+		ok(elem.children().get(0).tagName === "P", "(Single child - array parameter) We expect element to have P child element");
 		elem = $.create('div', null, [$.create('p'), $.create('span')]);
 		ok(elem.children('p').get(0).tagName === "P", "(Multiple child) We expect element to have P child element");
 		ok(elem.children('span').get(0).tagName === "SPAN", "(Multiple child) We expect element to have SPAN child element");
+		elem = $.create('div', null, null);
+		ok(elem.children().length === 0, "We expect null children parameter to be ignored");
 		elem = $.create('div', null, {one: $.create('p'), two: $.create('span')});
 		ok(elem.children().length === 0, "We expect malformed children parameter to be ignored (object)");
 		elem = $.create('div', null, 1);
-		ok(elem.children().length === 0, "We expect malformed children parameter to be ignored (int)");
+		ok(elem.children().length === 0, "We expect malformed children parameter to be ignored (number)");
+		elem = $.create('div', null, true);
+		ok(elem.children().length === 0, "We expect malformed children parameter to be ignored (boolean)");
 	});
 	
 	test("Root element", function () {
