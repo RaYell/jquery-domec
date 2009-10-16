be1d9af487906fce7a494a68776ea0ba47208d44
lukasz@rajchel.pl <lukasz@rajchel.pl>
2009-10-16 08:02:09 +0000

    spaces converted to tabs
    --HG--
    extra : convert_revision : svn%3A7d215080-934a-0410-a889-27081d6b4ed7/trunk%40127

--- jquery.domec.js
+++ jquery.domec.js
@@ -6,84 +6,83 @@
  * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
  * and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
  *
- * Documentation	:	http://code.google.com/p/jquery-domec/wiki/Documentation
- * Changelog		:	http://code.google.com/p/jquery-domec/wiki/Changelog
+ * Documentation    :    http://code.google.com/p/jquery-domec/wiki/Documentation
+ * Changelog        :    http://code.google.com/p/jquery-domec/wiki/Changelog
  */
 
 /*jslint white: true, onevar: true, undef: true, nomen: true, eqeqeq: true, plusplus: true, 
 bitwise: true, regexp: true, strict: true, newcap: true, immed: true, maxerr: 50, indent: 4 */
 /*global document, jQuery*/
-/*members DOMEC, addAttributes, addChildren, append, attr, create, createElement , extend, isArray,
-length, propertyIsEnumerable, splice, text, toString*/
+/*members DOMEC, addAttributes, addChildren, append, attr, create, createElement, each, extend, 
+isArray, length, text, toString*/
 'use strict';
 (function ($) {
 
-	// DOMEC Core class
-	$.DOMEC = (function () {
-		// variables declaration
-		// DOM element
-		var Element = (function () {
-			return {
-				// create element
-				create: function (name, root) {
-					// set default root if undefined
-					if (root === undefined || root === null) {
-						root = document;
-					}
+    // DOMEC Core class
+    $.DOMEC = (function () {
+        // variables declaration
+        // DOM element
+        var Element = (function () {
+            return {
+                // create element
+                create: function (name, root) {
+                    // set default root if undefined
+                    if (root === undefined || root === null) {
+                        root = document;
+                    }
 
-					if (typeof root === 'object' && !$.isArray(root) && 
-						typeof name === 'string') {
-						return $(root.createElement(name));
-					}
+                    if (typeof root === 'object' && !$.isArray(root) && 
+                        typeof name === 'string') {
+                        return $(root.createElement(name));
+                    }
 
-					return undefined;
-				},
-				// add attributes
-				addAttributes: function (elem, attributes) {
-					if (typeof attributes === 'object' && !$.isArray(attributes)) {
-						for (var key in attributes) {
-							if (typeof attributes[key] === 'string' || 
-								typeof attributes[key] === 'number') {
-								elem.attr(key, attributes[key]);
-							}
-						}
-					}
-				},
-				// add child elements
-				addChildren: function (elem, children) {
-					if (children !== undefined && children !== null) {
-						if ($.isArray(children)) {
-							for (var i = 0; i < children.length; i += 1) {
-								elem.append(children[i]);
-							}
-						} else if (typeof children === 'object') {
-							elem.append(children);
-						} else {
-							elem.text(children.toString());
-						}
-					}
-				}
-			};
-		}());
+                    return undefined;
+                },
+                // add attributes
+                addAttributes: function (elem, attr) {
+                    if (typeof attr === 'object' && attr !== null && !$.isArray(attr)) {
+                        $.each(attr, function (key, val) {
+                            if (typeof val === 'string' || typeof val === 'number') {
+                                elem.attr(key, val);
+                            }
+                        });
+                    }
+                },
+                // add child elements
+                addChildren: function (elem, children) {
+                    if (children !== undefined && children !== null) {
+                        if ($.isArray(children)) {
+                            $.each(children, function () {
+                                elem.append(this);
+                            });
+                        } else if (typeof children === 'object') {
+                            elem.append(children);
+                        } else {
+                            elem.text(children.toString());
+                        }
+                    }
+                }
+            };
+        }());
 
-		// DOMEC public members
-		return {
-			create: function (name, attributes, children, root) {
-				var elem = Element.create(name, root);
+        // DOMEC public members
+        return {
+            create: function (name, attributes, children, root) {
+                var elem = Element.create(name, root);
 
-				if (elem !== undefined) {
-					Element.addAttributes(elem, attributes);
-					Element.addChildren(elem, children);
-				}
+                if (elem !== undefined) {
+                    Element.addAttributes(elem, attributes);
+                    Element.addChildren(elem, children);
+                }
 
-				return elem;
-			}
-		};
-	}());
+                return elem;
+            }
+        };
+    }());
 
- 	// register jQuery extension
-	$.extend({
-		create: $.DOMEC.create
-	});
+     // register jQuery extension
+    $.extend({
+        create: $.DOMEC.create
+    });
 
 }(jQuery));
--- tests/tests.js
+++ tests/tests.js
@@ -5,116 +5,116 @@ bitwise: true, regexp: true, strict: true, newcap: true, immed: true, maxerr: 50
 ok, one, remove, tagName, testDone, text, two*/
 'use strict';
 $(function () {
-	// hack for html validator (ol cannot be empty)
-	$('li').remove();
-	
-	if (typeof fireunit === "object") {
+    // hack for html validator (ol cannot be empty)
+    $('li').remove();
+    
+    if (typeof fireunit === "object") {
         QUnit.log = fireunit.ok;
         QUnit.done = fireunit.testDone;
-	}
-	
-	test('Basic element creation', function () {
-		expect(8);
-		
-		var elem = $.create('div');
-		
-		ok(elem.get(0).tagName === 'DIV', 'Create element (string)');
-		elem = $.create(null);
-		ok(elem === undefined, 'Create element (null)');
-		elem = $.create(undefined);
-		ok(elem === undefined, 'Create element (undefined)');
-		elem = $.create({one: 'div'});
-		ok(elem === undefined, 'Create element (object)');
-		elem = $.create(['div']);
-		ok(elem === undefined, 'Create element (array)');
-		elem = $.create(1);
-		ok(elem === undefined, 'Create element (int)');
-		elem = $.create(1.2);
-		ok(elem === undefined, 'Create element (float)');
-		elem = $.create($);
-		ok(elem === undefined, 'Create element (boolean)');
-	});
-	
-	test('Attributes', function () {
-		expect(10);
-		
-		var elem = $.create('div', {id: 'testId'});
-		
-		same(elem.attr('id'), 'testId', 'Set single attribute');
-		elem = $.create('div', {id: 'testId', name: 'testName'});
-		same(elem.attr('id'), 'testId', 'Set multiple attributes. Check id');
-		same(elem.attr('name'), 'testName', 'Set multiple attributes. Check name');
-		elem = $.create('div', null);
-		ok(elem.attr('id') === '', 'Set attribute (null)');
-		elem = $.create('div', ['id: myId']);
-		ok(elem.attr('id') === '', 'Set attribute (array)');
-		elem = $.create('div', 'id: myId');
-		ok(elem.attr('id') === '', 'Set attribute (string)');
-		elem = $.create('div', 1);
-		ok(elem.attr('id') === '', 'Set attribute (int)');
-		elem = $.create('div', 1.2);
-		ok(elem.attr('id') === '', 'Set attribute (float)');
-		elem = $.create('div', true);
-		ok(elem.attr('id') === '', 'Set attribute (boolean)');
-		elem = $.create('div', $);
-		ok(elem.attr('id') === '', 'Set attribute (function)');
-	});
-	
-	test('Children', function () {
-		expect(11);
-		
-		var elem = $.create('div', null, 'testText');
-		
-		same(elem.text(), 'testText', 'Create text node');
-		elem = $.create('div', null, $.create('p'));
-		ok(elem.children().get(0).tagName === 'P', 'Create tree with single child (object)');
-		elem = $.create('div', null, [$.create('p')]);
-		ok(elem.children().get(0).tagName === 'P', 'Create tree with single child (array)');
-		elem = $.create('div', null, [$.create('p'), $.create('span')]);
-		ok(elem.children().get(0).tagName === 'P', 
-			'Create tree with several children. Get first child');
-		ok(elem.children().get(1).tagName === 'SPAN', 
-			'Create tree with several children. Get second child');
-		elem = $.create('div', null, null);
-		ok(elem.children().length === 0, 'Create element with child nodes (null)');
-		elem = $.create('div', null, {one: $.create('p'), two: $.create('span')});
-		ok(elem.children().length === 0, 'Create element with child nodes (object)');
-		elem = $.create('div', null, 1);
-		ok(elem.children().length === 0, 'Create element with child nodes (int)');
-		elem = $.create('div', null, 1);
-		ok(elem.children().length === 0, 'Create element with child nodes (float)');
-		elem = $.create('div', null, true);
-		ok(elem.children().length === 0, 'Create element with child nodes (boolean)');
-		elem = $.create('div', null, $);
-		ok(elem.children().length === 0, 'Create element with child nodes (function)');
-	});
-	
-	test('Root element', function () {
-		expect(10);
-		
-		var elem = $.create('div', null, null, null);
-		
-		ok(elem.get(0).tagName === 'DIV', 'Create element with given root (null)');
-		elem = $.create('div', null, null, undefined);
-		ok(elem.get(0).tagName === 'DIV', 'Create element with given root (undefined)');
-		elem = $.create('div', null, null, document);
-		ok(elem.get(0).tagName === 'DIV', 'Create element with given root (object)');
-		elem = $.create('div', null, null, 1);
-		ok(elem === undefined, 'Create element with given root (int)');
-		elem = $.create('div', null, null, 1.2);
-		ok(elem === undefined, 'Create element with given root (float)');
-		elem = $.create('div', null, null, 'document');
-		ok(elem === undefined, 'Create element with given root (string)');
-		elem = $.create('div', null, null, true);
-		ok(elem === undefined, 'Create element with given root (boolean)');
-		elem = $.create('div', null, null, []);
-		ok(elem === undefined, 'Create element with given root (array)');
-		elem = $.create('div', null, null, $);
-		ok(elem === undefined, 'Create element with given root (function)');
-		
-		$.create('iframe', {id: 'myIframe'}).insertAfter('#main');
-		elem = $.create('div', null, null, $('iframe').get(0).contentDocument);
-		ok(elem.get(0).tagName === 'DIV', 'Create element in iFrame');
-		$('iframe#myIframe').remove();
-	});
+    }
+    
+    test('Basic element creation', function () {
+        expect(8);
+        
+        var elem = $.create('div');
+        
+        ok(elem.get(0).tagName === 'DIV', 'Create element (string)');
+        elem = $.create(null);
+        ok(elem === undefined, 'Create element (null)');
+        elem = $.create(undefined);
+        ok(elem === undefined, 'Create element (undefined)');
+        elem = $.create({one: 'div'});
+        ok(elem === undefined, 'Create element (object)');
+        elem = $.create(['div']);
+        ok(elem === undefined, 'Create element (array)');
+        elem = $.create(1);
+        ok(elem === undefined, 'Create element (int)');
+        elem = $.create(1.2);
+        ok(elem === undefined, 'Create element (float)');
+        elem = $.create($);
+        ok(elem === undefined, 'Create element (boolean)');
+    });
+    
+    test('Attributes', function () {
+        expect(10);
+        
+        var elem = $.create('div', {id: 'testId'});
+        
+        same(elem.attr('id'), 'testId', 'Set single attribute');
+        elem = $.create('div', {id: 'testId', name: 'testName'});
+        same(elem.attr('id'), 'testId', 'Set multiple attributes. Check id');
+        same(elem.attr('name'), 'testName', 'Set multiple attributes. Check name');
+        elem = $.create('div', null);
+        ok(elem.attr('id') === '', 'Set attribute (null)');
+        elem = $.create('div', ['id: myId']);
+        ok(elem.attr('id') === '', 'Set attribute (array)');
+        elem = $.create('div', 'id: myId');
+        ok(elem.attr('id') === '', 'Set attribute (string)');
+        elem = $.create('div', 1);
+        ok(elem.attr('id') === '', 'Set attribute (int)');
+        elem = $.create('div', 1.2);
+        ok(elem.attr('id') === '', 'Set attribute (float)');
+        elem = $.create('div', true);
+        ok(elem.attr('id') === '', 'Set attribute (boolean)');
+        elem = $.create('div', $);
+        ok(elem.attr('id') === '', 'Set attribute (function)');
+    });
+    
+    test('Children', function () {
+        expect(11);
+        
+        var elem = $.create('div', null, 'testText');
+        
+        same(elem.text(), 'testText', 'Create text node');
+        elem = $.create('div', null, $.create('p'));
+        ok(elem.children().get(0).tagName === 'P', 'Create tree with single child (object)');
+        elem = $.create('div', null, [$.create('p')]);
+        ok(elem.children().get(0).tagName === 'P', 'Create tree with single child (array)');
+        elem = $.create('div', null, [$.create('p'), $.create('span')]);
+        ok(elem.children().get(0).tagName === 'P', 
+            'Create tree with several children. Get first child');
+        ok(elem.children().get(1).tagName === 'SPAN', 
+            'Create tree with several children. Get second child');
+        elem = $.create('div', null, null);
+        ok(elem.children().length === 0, 'Create element with child nodes (null)');
+        elem = $.create('div', null, {one: $.create('p'), two: $.create('span')});
+        ok(elem.children().length === 0, 'Create element with child nodes (object)');
+        elem = $.create('div', null, 1);
+        ok(elem.children().length === 0, 'Create element with child nodes (int)');
+        elem = $.create('div', null, 1);
+        ok(elem.children().length === 0, 'Create element with child nodes (float)');
+        elem = $.create('div', null, true);
+        ok(elem.children().length === 0, 'Create element with child nodes (boolean)');
+        elem = $.create('div', null, $);
+        ok(elem.children().length === 0, 'Create element with child nodes (function)');
+    });
+    
+    test('Root element', function () {
+        expect(10);
+        
+        var elem = $.create('div', null, null, null);
+        
+        ok(elem.get(0).tagName === 'DIV', 'Create element with given root (null)');
+        elem = $.create('div', null, null, undefined);
+        ok(elem.get(0).tagName === 'DIV', 'Create element with given root (undefined)');
+        elem = $.create('div', null, null, document);
+        ok(elem.get(0).tagName === 'DIV', 'Create element with given root (object)');
+        elem = $.create('div', null, null, 1);
+        ok(elem === undefined, 'Create element with given root (int)');
+        elem = $.create('div', null, null, 1.2);
+        ok(elem === undefined, 'Create element with given root (float)');
+        elem = $.create('div', null, null, 'document');
+        ok(elem === undefined, 'Create element with given root (string)');
+        elem = $.create('div', null, null, true);
+        ok(elem === undefined, 'Create element with given root (boolean)');
+        elem = $.create('div', null, null, []);
+        ok(elem === undefined, 'Create element with given root (array)');
+        elem = $.create('div', null, null, $);
+        ok(elem === undefined, 'Create element with given root (function)');
+        
+        $.create('iframe', {id: 'myIframe'}).insertAfter('#main');
+        elem = $.create('div', null, null, $('iframe').get(0).contentDocument);
+        ok(elem.get(0).tagName === 'DIV', 'Create element in iFrame');
+        $('iframe#myIframe').remove();
+    });
 });
