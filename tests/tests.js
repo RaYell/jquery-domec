a053131505372fb47e3224f3fe21cff164129c02
lrajchel <none@none>
2011-09-16 12:21:19 +0200

    JQUnit replaced by QUnit because the former is no longer maintained

--- tests/tests.js
+++ tests/tests.js
@@ -1,8 +1,9 @@
 /*jslint white: true, onevar: true, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, 
 regexp: true, strict: true, newcap: true, immed: true, maxerr: 50, indent: 4, maxlen: 120*/
-/*global $, jqUnit, document*/
-/*members attr, attributes, children, click, contentDocument, create, done, equals, events, expect, get, id, 
-insertAfter, length, log, name, ok, one, remove, root, tagName, testDone, trigger, text, two*/
+/*global $, document, equals, expect, ok, test*/
+/*members attr, attributes, children, click, contentDocument, create, done, events, get, id, insertAfter, length, log, 
+name, one, remove, root, tagName, testDone, trigger, text, two*/
+
 $(function () {
 	'use strict';
     var testVal, isEmptyOrUndefined;
@@ -14,55 +15,55 @@ $(function () {
 		return value === '' || value === undefined;
 	};
     
-    jqUnit.test('Basic element creation', function () {
-        jqUnit.expect(8);
+    test('Basic element creation', function () {
+        expect(8);
         
         var elem = $.create('div');
         
-        jqUnit.equals(elem.get(0).tagName, 'DIV', 'Create element (string)');
+        equals(elem.get(0).tagName, 'DIV', 'Create element (string)');
         elem = $.create(null);
-        jqUnit.equals(elem, undefined, 'Create element (null)');
+        equals(elem, undefined, 'Create element (null)');
         elem = $.create(undefined);
-        jqUnit.equals(elem, undefined, 'Create element (undefined)');
+        equals(elem, undefined, 'Create element (undefined)');
         elem = $.create({one: 'div'});
-        jqUnit.equals(elem, undefined, 'Create element (object)');
+        equals(elem, undefined, 'Create element (object)');
         elem = $.create(['div']);
-        jqUnit.equals(elem, undefined, 'Create element (array)');
+        equals(elem, undefined, 'Create element (array)');
         elem = $.create(1);
-        jqUnit.equals(elem, undefined, 'Create element (int)');
+        equals(elem, undefined, 'Create element (int)');
         elem = $.create(1.2);
-        jqUnit.equals(elem, undefined, 'Create element (float)');
+        equals(elem, undefined, 'Create element (float)');
         elem = $.create($);
-        jqUnit.equals(elem, undefined, 'Create element (boolean)');
+        equals(elem, undefined, 'Create element (boolean)');
     });
     
-    jqUnit.test('Attributes', function () {
-        jqUnit.expect(10);
+    test('Attributes', function () {
+        expect(10);
         
         var elem = $.create('div', {attributes: {id: 'testId'}});
         
-        jqUnit.equals(elem.attr('id'), 'testId', 'Set single attribute');
+        equals(elem.attr('id'), 'testId', 'Set single attribute');
         elem = $.create('div', {attributes: {id: 'testId', name: 'testName'}});
-        jqUnit.equals(elem.attr('id'), 'testId', 'Set multiple attributes. Check id');
-        jqUnit.equals(elem.attr('name'), 'testName', 'Set multiple attributes. Check name');
+        equals(elem.attr('id'), 'testId', 'Set multiple attributes. Check id');
+        equals(elem.attr('name'), 'testName', 'Set multiple attributes. Check name');
         elem = $.create('div', {attributes: null});
-        jqUnit.ok(isEmptyOrUndefined(elem.attr('id')), 'Set attribute (null)');
+        ok(isEmptyOrUndefined(elem.attr('id')), 'Set attribute (null)');
         elem = $.create('div', {attributes: ['id: myId']});
-        jqUnit.ok(isEmptyOrUndefined(elem.attr('id')), 'Set attribute (array)');
+        ok(isEmptyOrUndefined(elem.attr('id')), 'Set attribute (array)');
         elem = $.create('div', {attributes: 'id: myId'});
-        jqUnit.ok(isEmptyOrUndefined(elem.attr('id')), 'Set attribute (string)');
+        ok(isEmptyOrUndefined(elem.attr('id')), 'Set attribute (string)');
         elem = $.create('div', {attributes: 1});
-        jqUnit.ok(isEmptyOrUndefined(elem.attr('id')), 'Set attribute (int)');
+        ok(isEmptyOrUndefined(elem.attr('id')), 'Set attribute (int)');
         elem = $.create('div', {attributes: 1.2});
-        jqUnit.ok(isEmptyOrUndefined(elem.attr('id')), 'Set attribute (float)');
+        ok(isEmptyOrUndefined(elem.attr('id')), 'Set attribute (float)');
         elem = $.create('div', {attributes: true});
-        jqUnit.ok(isEmptyOrUndefined(elem.attr('id')), 'Set attribute (boolean)');
+        ok(isEmptyOrUndefined(elem.attr('id')), 'Set attribute (boolean)');
         elem = $.create('div', {attributes: $});
-        jqUnit.ok(isEmptyOrUndefined(elem.attr('id')), 'Set attribute (function)');
+        ok(isEmptyOrUndefined(elem.attr('id')), 'Set attribute (function)');
     });
     
-    jqUnit.test('Event handlers', function () {
-        jqUnit.expect(1);
+    test('Event handlers', function () {
+        expect(1);
         
         var elem = $.create('div', {events: {
             click: function () {
@@ -72,62 +73,62 @@ $(function () {
         
         elem.trigger('click');
         
-        jqUnit.equals(testVal, 123, 'Create element with click event handler');
+        equals(testVal, 123, 'Create element with click event handler');
     });
     
-    jqUnit.test('Children', function () {
-        jqUnit.expect(11);
+    test('Children', function () {
+        expect(11);
         
         var elem = $.create('div', {children: 'testText'});
         
-        jqUnit.equals(elem.text(), 'testText', 'Create text node');
+        equals(elem.text(), 'testText', 'Create text node');
         elem = $.create('div', {children: $.create('p')});
-        jqUnit.equals(elem.children().get(0).tagName, 'P', 'Create tree with single child (object)');
+        equals(elem.children().get(0).tagName, 'P', 'Create tree with single child (object)');
         elem = $.create('div', {children: [$.create('p')]});
-        jqUnit.equals(elem.children().get(0).tagName, 'P', 'Create tree with single child (array)');
+        equals(elem.children().get(0).tagName, 'P', 'Create tree with single child (array)');
         elem = $.create('div', {children: [$.create('p'), $.create('span')]});
-        jqUnit.equals(elem.children().get(0).tagName, 'P', 'Create tree with several children. Get first child');
-        jqUnit.equals(elem.children().get(1).tagName, 'SPAN', 'Create tree with several children. Get second child');
+        equals(elem.children().get(0).tagName, 'P', 'Create tree with several children. Get first child');
+        equals(elem.children().get(1).tagName, 'SPAN', 'Create tree with several children. Get second child');
         elem = $.create('div', {children: null});
-        jqUnit.equals(elem.children().length, 0, 'Create element with child nodes (null)');
+        equals(elem.children().length, 0, 'Create element with child nodes (null)');
         elem = $.create('div', {children: {one: $.create('p'), two: $.create('span')}});
-        jqUnit.equals(elem.children().length, 0, 'Create element with child nodes (object)');
+        equals(elem.children().length, 0, 'Create element with child nodes (object)');
         elem = $.create('div', {children: 1});
-        jqUnit.equals(elem.children().length, 0, 'Create element with child nodes (int)');
+        equals(elem.children().length, 0, 'Create element with child nodes (int)');
         elem = $.create('div', {children: 1.2});
-        jqUnit.equals(elem.children().length, 0, 'Create element with child nodes (float)');
+        equals(elem.children().length, 0, 'Create element with child nodes (float)');
         elem = $.create('div', {children: true});
-        jqUnit.equals(elem.children().length, 0, 'Create element with child nodes (boolean)');
+        equals(elem.children().length, 0, 'Create element with child nodes (boolean)');
         elem = $.create('div', {children: $});
-        jqUnit.equals(elem.children().length, 0, 'Create element with child nodes (function)');
+        equals(elem.children().length, 0, 'Create element with child nodes (function)');
     });
     
-    jqUnit.test('Root element', function () {
-        jqUnit.expect(10);
+    test('Root element', function () {
+        expect(10);
         
         var elem = $.create('div', {root: null});
         
-        jqUnit.equals(elem.get(0).tagName, 'DIV', 'Create element with given root (null)');
+        equals(elem.get(0).tagName, 'DIV', 'Create element with given root (null)');
         elem = $.create('div', {root: undefined});
-        jqUnit.equals(elem.get(0).tagName, 'DIV', 'Create element with given root (undefined)');
+        equals(elem.get(0).tagName, 'DIV', 'Create element with given root (undefined)');
         elem = $.create('div', {root: document});
-        jqUnit.equals(elem.get(0).tagName, 'DIV', 'Create element with given root (object)');
+        equals(elem.get(0).tagName, 'DIV', 'Create element with given root (object)');
         elem = $.create('div', {root: 1});
-        jqUnit.equals(elem, undefined, 'Create element with given root (int)');
+        equals(elem, undefined, 'Create element with given root (int)');
         elem = $.create('div', {root: 1.2});
-        jqUnit.equals(elem, undefined, 'Create element with given root (float)');
+        equals(elem, undefined, 'Create element with given root (float)');
         elem = $.create('div', {root: 'document'});
-        jqUnit.equals(elem, undefined, 'Create element with given root (string)');
+        equals(elem, undefined, 'Create element with given root (string)');
         elem = $.create('div', {root: true});
-        jqUnit.equals(elem, undefined, 'Create element with given root (boolean)');
+        equals(elem, undefined, 'Create element with given root (boolean)');
         elem = $.create('div', {root: []});
-        jqUnit.equals(elem, undefined, 'Create element with given root (array)');
+        equals(elem, undefined, 'Create element with given root (array)');
         elem = $.create('div', {root: $});
-        jqUnit.equals(elem, undefined, 'Create element with given root (function)');
+        equals(elem, undefined, 'Create element with given root (function)');
         
-        $.create('iframe', {attributes: {id: 'myIframe'}}).insertAfter('#main');
+        $.create('iframe', {attributes: {id: 'myIframe'}}).insertAfter('#qunit-fixture');
         elem = $.create('div', {root: $('iframe').get(0).contentDocument});
-        jqUnit.equals(elem.get(0).tagName, 'DIV', 'Create element in iFrame');
+        equals(elem.get(0).tagName, 'DIV', 'Create element in iFrame');
         
         $('iframe#myIframe').remove();
     });
