acd0a8103869e00cc2b3a6cb0867f3d19156601b
Lukasz Rajchel <lukasz@rajchel.pl>
2014-05-05 14:21:00 +0200

    blanket code coverage added

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,23 @@
-module.exports = function(grunt) {
-	grunt.loadNpmTasks('grunt-contrib-qunit');
-	
-	grunt.initConfig({
-  		qunit: {
-    		all: ['tests/*.html']
-  		}
-	});
-	
-	grunt.registerTask('default', ['qunit']);
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-blanket-qunit');
+
+    grunt.initConfig({
+        qunit: {
+            all: ['tests/*.html']
+        },
+        blanket_qunit: {
+            all: {
+                options: {
+                    urls: [
+                        'tests/index.html?coverage=true&gruntReport',
+                        'tests/jquery2.html?coverage=true&gruntReport'
+                    ],
+                    threshold: 70
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['qunit', 'blanket_qunit']);
 };
